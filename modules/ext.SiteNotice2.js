3dd7907624022eb5939d16717004005c05d8deaf
DarkGames26 <fredericpadilla26@gmail.com>
2024-02-18 20:25:32 -0400

    JS Resource Changes

--- modules/ext.SiteNotice2.js
+++ modules/ext.SiteNotice2.js
@@ -1,39 +1,38 @@
-( function () {
-
-	var cookieName = 'dismissSiteNotice',
-		siteNoticeId = mw.config.get( 'wgSiteNoticeId' );
-
+  (() => {
+	const cookieName = 'dismissSiteNotice';
+	const siteNoticeId = mw.config.get('wgSiteNoticeId');
+  
 	// If no siteNoticeId is set, exit.
-	if ( !siteNoticeId ) {
-		return;
+	if (!siteNoticeId) {
+	  return;
 	}
-
+  
 	// If the user has the notice dismissal cookie set, exit.
-	if ( $.cookie( cookieName ) === siteNoticeId ) {
-		return;
+	if ($.cookie(cookieName) === siteNoticeId) {
+	  return;
 	}
-
+  
 	// Otherwise, show the notice ...
-	mw.util.addCSS( '.client-js .sitenotice2 { display: flex; }' );
-
+	mw.util.addCSS('.client-js .mw-sitenotice2 { display: flex; }');
+  
 	// ... and enable the dismiss button.
-	$( function () {
-		// eslint-disable-next-line no-jquery/no-global-selector
-		$( '.mw-dismissable-notice-close' )
-			.css( 'visibility', 'visible' )
-			.find( 'a' )
-			.on( 'click keypress', function ( e ) {
-				if (
-					e.type === 'click' ||
-					e.type === 'keypress' && e.which === 13
-				) {
-					$( this ).closest( '.sitenotice2' ).hide();
-					$.cookie( cookieName, siteNoticeId, {
-						expires: 30,
-						path: '/'
-					} );
-				}
-			} );
-	} );
-
-}() );
+	$(() => {
+	  // eslint-disable-next-line no-jquery/no-global-selector
+	  $('.mw-dismissable-notice-close-button')
+		.css('visibility', 'visible')
+		.find('a')
+		.on('click keypress', (e) => {
+		  if (
+			e.type === 'click' ||
+			(e.type === 'keypress' && e.which === 13)
+		  ) {
+			$(this).closest('.mw-sitenotice2').hide();
+			$.cookie(cookieName, siteNoticeId, {
+			  expires: 30,
+			  path: '/',
+			});
+		  }
+		});
+	});
+  })();
+  
