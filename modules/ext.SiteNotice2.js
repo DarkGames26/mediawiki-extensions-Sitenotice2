21989ec85a188cb49d3cf61be558ec9a7eecedab
DarkGames26 <fredericpadilla26@gmail.com>
2024-02-18 20:42:07 -0400

    Fixed the error that the warning was not removed when clicked

--- modules/ext.SiteNotice2.js
+++ modules/ext.SiteNotice2.js
@@ -1,38 +1,39 @@
-  (() => {
-	const cookieName = 'dismissSiteNotice';
-	const siteNoticeId = mw.config.get('wgSiteNoticeId');
-  
-	// If no siteNoticeId is set, exit.
-	if (!siteNoticeId) {
-	  return;
-	}
-  
-	// If the user has the notice dismissal cookie set, exit.
-	if ($.cookie(cookieName) === siteNoticeId) {
-	  return;
-	}
-  
-	// Otherwise, show the notice ...
-	mw.util.addCSS('.client-js .mw-sitenotice2 { display: flex; }');
-  
-	// ... and enable the dismiss button.
-	$(() => {
-	  // eslint-disable-next-line no-jquery/no-global-selector
-	  $('.mw-dismissable-notice-close')
-		.css('visibility', 'visible')
-		.find('a')
-		.on('click keypress', (e) => {
-		  if (
-			e.type === 'click' ||
-			(e.type === 'keypress' && e.which === 13)
-		  ) {
-			$(this).closest('.mw-sitenotice2').hide();
-			$.cookie(cookieName, siteNoticeId, {
-			  expires: 30,
-			  path: '/',
-			});
-		  }
-		});
-	});
-  })();
-  
+(() => {
+    const cookieName = 'dismissSiteNotice';
+    const siteNoticeId = mw.config.get('wgSiteNoticeId');
+
+    // If no siteNoticeId is set, exit.
+    if (!siteNoticeId) {
+        return;
+    }
+
+    // If the user has the notice dismissal cookie set, exit.
+    if ($.cookie(cookieName) === siteNoticeId) {
+        return;
+    }
+
+    // Otherwise, show the notice ...
+    mw.util.addCSS('.client-js .mw-sitenotice2 { display: flex; }');
+
+    // ... and enable the dismiss button.
+    $(() => {
+        // eslint-disable-next-line no-jquery/no-global-selector
+        $('.mw-dismissable-notice-close')
+            .css('visibility', 'visible')
+            .find('a')
+            .on('click keypress', function(e) {
+                if (
+                    e.type === 'click' ||
+                    (e.type === 'keypress' && e.which === 13)
+                ) {
+                    $(this).closest('.mw-sitenotice2').fadeOut(200, function() {
+                        $(this).css('display', 'none'); // Close
+                    });
+                    $.cookie(cookieName, siteNoticeId, {
+                        expires: 30,
+                        path: '/',
+                    });
+                }
+            });
+    });
+})();
