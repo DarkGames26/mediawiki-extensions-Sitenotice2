f023dc98043851f8cc26c0e5a0a880cf557d9a86
DarkGames26 <fredericpadilla26@gmail.com>
2024-03-07 00:39:32 -0400

    Cosmos Skin JS

--- modules/ext.CosmosSiteNotice.js
+++ modules/ext.CosmosSiteNotice.js
@@ -1,6 +1,6 @@
 // Eliminar la implementación del Sitenotice de Cosmos
-$('#siteNotice').prependTo($('#cosmos-pageContent-subtitle')).then(function() {
-    // Mover el elemento fuera de la implementación de Cosmos
-    $('#cosmos-content-siteNotice').remove(); // Eliminar el elemento y sus hijos en cuestión
-  });
-  
+$('#siteNotice').prependTo($('#cosmos-pageContent-subtitle')).promise().done(function() {
+  // Esta función se ejecuta después de que la operación prependTo() haya terminado
+  // Mover el elemento fuera de la implementación de Cosmos
+  $('#cosmos-content-siteNotice').remove(); // Eliminar el elemento y sus hijos en cuestión
+});
