56f1c5ccb0f0e127334ba7af99de722af296f6be
DarkGames26 <fredericpadilla26@gmail.com>
2024-03-06 20:34:23 -0400

    Adding animation for the notice, and updating Cosmos skin JS.

--- modules/ext.CosmosSiteNotice.js
+++ modules/ext.CosmosSiteNotice.js
@@ -1,3 +1,6 @@
 // Eliminar la implementación del Sitenotice de Cosmos
-$('#siteNotice').prependTo($('#cosmos-pageContent-subtitle')); // Mover el elemento fuera de la implementación de Cosmos
-$('#cosmos-content-siteNotice').remove(); // Eliminar el elemento y sus hijos en cuestión
+$('#siteNotice').prependTo($('#cosmos-pageContent-subtitle')).then(function() {
+    // Mover el elemento fuera de la implementación de Cosmos
+    $('#cosmos-content-siteNotice').remove(); // Eliminar el elemento y sus hijos en cuestión
+  });
+  
